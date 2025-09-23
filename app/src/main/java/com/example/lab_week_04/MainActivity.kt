98e5a874213b31e46520a8bc9e6f20fb4287a2f3
ul.auliaaa <163988764+auliaaaaidr@users.noreply.github.com>
2025-09-23 21:31:38 +0700

    Commit No. 2 – Bottom Navigation

--- app/src/main/java/com/example/lab_week_04/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_04/MainActivity.kt
@@ -1,34 +1,54 @@
 package com.example.lab_week_04
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.*
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         setSupportActionBar(findViewById(R.id.toolbar))
+
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-                    as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        //Creating top level destinations
-        //and adding them to the draw
+
+        // Top level destinations (fragment utama)
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.listFragment, R.id.favoritesFragment
-            ), findViewById(R.id.drawer_layout)
+                R.id.listFragment,
+                R.id.favoritesFragment,
+                R.id.cafeFragment
+            ),
+            findViewById(R.id.drawer_layout)
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Hubungkan Navigation Drawer
         findViewById<NavigationView>(R.id.nav_view)
             ?.setupWithNavController(navController)
+
+        // Hubungkan Bottom Navigation
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            ?.setupWithNavController(navController)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) ||
-                super.onSupportNavigateUp()
+        // Gunakan NavigationUI supaya tetap pakai appBarConfiguration
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
