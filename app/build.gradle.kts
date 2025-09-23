770b783c1ea42461d98c5808361d1ecde836f857
ul.auliaaa <163988764+auliaaaaidr@users.noreply.github.com>
2025-09-23 20:16:23 +0700

    Commit No. 1 – Navigation drawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
